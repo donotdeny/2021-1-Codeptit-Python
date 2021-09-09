e1e62bacc8acf8fad3fb81f7d60a76009114d3c1
buffaloyoungcoding <78521392+buffaloyoungcoding@users.noreply.github.com>
2021-09-09 18:00:25 +0700

    Create ex41.py

---
+++ ex41.py
@@ -0,0 +1,25 @@
+def mulOfDigits(n):
+    sum = 1
+    while(n > 0):
+        sum *= n %10
+        n = int(n/10)
+    return sum
+
+t = int(input())
+while(t > 0):
+    n = int(input())
+    arr = [int(i) for i in input().split()]
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            if(mulOfDigits(arr[i]) > mulOfDigits(arr[j])):
+                tmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = tmp
+            if(mulOfDigits(arr[i]) == mulOfDigits(arr[j]) and arr[i] > arr[j]):
+                tmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = tmp
+    for i in range(n):
+        print(arr[i], end = " ")
+    print()
+    t -= 1
